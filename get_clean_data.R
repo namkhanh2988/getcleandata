9d902a688190a248b33b90244f7d74ec025dcfac
Khanh Nguyen <namkhanh2988@gmail.com>
2016-02-14 19:45:12 -0800

    Not printing row name in output file

--- get_clean_data.R
+++ get_clean_data.R
@@ -66,4 +66,4 @@ colnames(harData) <- colNames
 # 5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
 library(plyr)
 tidyData = ddply(harData, c("subject","activity"), numcolwise(mean))
-write.table(tidyData, file = "tidy_data.txt")
+write.table(tidyData, file = "tidy_data.txt", row.names = FALSE)
